94484f78039254d8d33c40fe134bf0542d454a1a
ghilo17 <61m37.17@gmail.com>
2017-03-10 09:50:29 +0800

    Fix loader app

--- loader.php
+++ loader.php
@@ -2,4 +2,4 @@
 			clearInterval(m);
 </script>
 <?php header('Access-Control-Allow-Origin: *'); ?>
-<?php exec('"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Default" --app-id=pgahiajkdfdgjgjeknggbbbkopafndfo"');?>
+<?php exec('"C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory=Default --app-id=pgahiajkdfdgjgjeknggbbbkopafndfo"');?>
